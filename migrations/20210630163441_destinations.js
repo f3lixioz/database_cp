acb4432d476edd4a22a69bf8017f2fc15b4c6de4
f3lixioz <f3lixioz@gmail.com>
2021-06-30 17:11:28 -0600

    Db generated

--- migrations/20210630163441_destinations.js
+++ migrations/20210630163441_destinations.js
@@ -1,23 +1,23 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable(
-    'destinations', table => {
-        table.increments('id');
-        table.string('name');
-        table.float('average_temp');
-        table.boolean('has_beaches');
-        table.boolean('has_mountains');
-        table.float('cost_of_flight');
-    },
-    'airlines', table => {
-        table.increments('id');
-        table.string('name');
-        table.string(['destinations_flown_to']);
-    }
-    );
-    
+        'destinations', table => {
+            table.increments('id');
+            table.string('name');
+            table.float('average_temp');
+            table.boolean('has_beaches');
+            table.boolean('has_mountains');
+            table.float('cost_of_flight');
+        })
+        .createTable(
+            'airlines', table => {
+                table.increments('id');
+                table.string('name');
+                table.specificType('destinations_flown_to','varchar[]');
+            }
+        );    
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('destinations','airlines');
+    return knex.schema.dropTableIfExists('destinations').dropTableIfExists('airlines');
 };
